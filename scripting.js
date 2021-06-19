6efa88bb50f06029d88785b38c44453e318d04ae
idcossa <85403954+idcossa@users.noreply.github.com>
2021-06-19 23:40:13 +0200

    Metadata Catalogue
    Metadata for Transnet Transport network

--- scripting.js
+++ scripting.js
@@ -54,8 +54,33 @@ img.onclick = function(){
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
+//For Image 7//
+var img = document.getElementById("roads");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
 //Closing of Modal//
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modal.style.display = "none";
+}
+
+//Search Box//
+function search_metadatabox() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('metadatabox');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
 }
